00903b4a0d0379ae8438707e9026edc2a4691e90
Ana Nikolova <ani.nik19@gmail.com>
2019-09-14 07:06:51 +0200

    build the reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,15 @@
+const songsReducer = () => {
+  return [
+    { title: "No Scrubs", duration: "4:50" },
+    { title: "Macarena", duration: "3:20" },
+    { title: "All Start", duration: "5:34" },
+    { title: "I Want It That Way", duration: "4:27" }
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === "SONGS_SELECTED") {
+    return action.payload;
+  }
+  return selectedSong;
+};
